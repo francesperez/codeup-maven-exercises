2849d8e1969fe58454dcb753cea56d3a12ec15e6
francesperez <frances.perez141@gmail.com>
2022-11-02 16:07:10 -0500

    Completed test-driven development cycle warm-up exercise

---
+++ src/main/java/CodeupCryptTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class CodeupCryptTest {
+
+    @Test
+    public static void testIfVersionIsSet(){
+        assertEquals(0.0, CodeupCrypt.version, 0);
+        CodeupCrypt.version = 1.2;
+        assertEquals(1.2, CodeupCrypt.version, 0);
+    }
+
+    @Test
+    public void testHashPassword(){
+        assertEquals("C0d39p", CodeupCrypt.hashPassword("Codeup"));
+        assertEquals("F3r", CodeupCrypt.hashPassword("Fer"));
+        assertEquals("123", CodeupCrypt.hashPassword("123"));
+        assertEquals("124", CodeupCrypt.hashPassword("12a"));
+    }
+
+    @Test
+    public void testCheckPassword(){
+        assertTrue("check password", CodeupCrypt.checkPassword("fer", "f3r"));
+        assertFalse("check password", CodeupCrypt.checkPassword("fer", "fer"));
+    }
+
+}
