1c2c742a5f2f63db665fd0f8e38e563d421ce7c3
francesperez <frances.perez141@gmail.com>
2022-11-02 13:37:35 -0500

    Testing Code with jUnit pre-lunch lecture

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void additionalMethodTest() {
+        int input1 = 20;
+        int input2 = 30;
+        int expected = 50;
+        assertEquals(expected, MathOperations.add(input1, input2));
+    }
+
+}
